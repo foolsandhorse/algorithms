8f87aa1cf8d0db6fce220f98b611102e92e4c130
foolsandhorse <norberthenry@outlook.com>
2016-10-26 11:16:13 -0700

    Initial Commit

---
+++ algorithms/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  algorithms
+//
+//  Created by Norbert Henry on 10/26/16.
+//  Copyright © 2016 Norbert Henry. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+	@autoreleasepool {
+	    // insert code here...
+	    NSLog(@"Hello, World!");
+	}
+    return 0;
+}
